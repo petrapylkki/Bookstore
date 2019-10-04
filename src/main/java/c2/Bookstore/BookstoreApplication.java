5286ac8f566f20560acc3a79b3234acd0798925d
petra.pylkki <petra.pylkki@hotmail.com>
2019-10-04 21:47:36 +0300

    Web security with user entity

--- src/main/java/c2/Bookstore/BookstoreApplication.java
+++ src/main/java/c2/Bookstore/BookstoreApplication.java
@@ -11,6 +11,8 @@ import c2.Bookstore.domain.Book;
 import c2.Bookstore.domain.BookRepository;
 import c2.Bookstore.domain.Category;
 import c2.Bookstore.domain.CategoryRepository;
+import c2.Bookstore.domain.User;
+import c2.Bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -23,7 +25,7 @@ public class BookstoreApplication {
 
 	//testidatan luonti H2-testitietokantaan aina sovelluksen käynnistyessä
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository bookRepository, CategoryRepository categoryRepo) { 
+	public CommandLineRunner bookDemo(BookRepository bookRepository, CategoryRepository categoryRepo, UserRepository userRepo) { 
 		return (args) -> {
 			log.info("save few books");
 			categoryRepo.save(new Category("Lasten- ja nuortenkirjat"));
@@ -33,6 +35,12 @@ public class BookstoreApplication {
 			bookRepository.save(new Book("Muumipappa merellä", "Tove Jansson", "1234-A", 1998, 23, categoryRepo.findByName("Lasten- ja nuortenkirjat").get(0)));
 			bookRepository.save(new Book("Seitsemän veljestä", "Aleksis Kivi", "4321-B", 1932, 18, categoryRepo.findByName("Kaunokirjallisuus").get(0)));
 			
+			// Create users: admin/admin user/user
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			userRepo.save(user1);
+			userRepo.save(user2);
+						
 			log.info("fetch all books");
 			for (Book book : bookRepository.findAll()) {
 				log.info(book.toString());
---
+++ src/main/java/c2/Bookstore/domain/UserRepository.java
@@ -0,0 +1,7 @@
+package c2.Bookstore.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+	User findByUsername(String username);
+}
